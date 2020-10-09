55f813bbbb460e5f15262bd1405289fa8331651f
Trenton Potgieter <darkreapyre@gmail.com>
2020-10-09 12:11:27 -0700

    fixing typo

--- scriptv1.sh
+++ scriptv1.sh
@@ -65,7 +65,9 @@ managedNodeGroups:
   iam:
     withAddonPolicies:
       albIngress: true
-availabilityZones: [["${AWS_REGION}a", "${AWS_REGION}b", "${AWS_REGION}c"]
+
+availabilityZones: ["${AWS_REGION}a", "${AWS_REGION}b", "${AWS_REGION}c"]
+
 secretsEncryption:
   keyARN: ${MASTER_ARN}
 EOF
